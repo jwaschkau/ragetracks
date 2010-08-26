16ef9527faca2cabd7e78a6190e37d3efbbba628
Jannik <blazingsun@blazingsun-laptop>
2010-08-26 15:25:14 +0200

    renamed main to ode_test.py

---
+++ ode_test.py
@@ -0,0 +1,19 @@
+from direct.showbase.ShowBase import ShowBase
+from pandac.PandaModules import *
+ 
+
+class RageTracks(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+        #Initialise the Ode world
+        world = OdeWorld()
+        world.setGravity(0, 0, -9.81)
+         
+
+    def physicsTask(task):
+      # Step the simulation and set the new positions
+      world.quickStep(globalClock.getDt())
+      return task.cont
+
+game = RageTracks() 
+game.run()
