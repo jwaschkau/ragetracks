06c45c0f527b275831abae1f9575947ff1b2db93
Jannik <blazingsun@blazingsun-laptop>
2010-08-26 16:52:44 +0200

    switched model directory

--- ode_test.py
+++ ode_test.py
@@ -6,32 +6,47 @@ class RageTracks(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
         base.disableMouse()
-
+        
+        #initialise the lights
+        alight = AmbientLight('alight')
+        alight.setColor(VBase4(0.2, 0.2, 0.2, 1))
+        alnp = render.attachNewNode(alight)
+        render.setLight(alnp)
+        
+        #Initialise the Ode world
+        world = OdeWorld()
+        world.setGravity(0, 0, -9.81)
+        
         #load the models
         
         #environment
-        self.environ = self.loader.loadModel("data/models/egg/Plane")
+        self.environ = self.loader.loadModel("data/models/Plane")
         # Reparent the model to render.
         self.environ.reparentTo(self.render)
         # Apply scale and position transforms on the model.
-        self.environ.setScale(1, 1, 1)
+        self.environ.setScale(10, 10, 10)
         self.environ.setPos(0, 0, 0)
         
         #racer
-        self.glider = self.loader.loadModel("data/models/egg/gleiter_A")
+        self.glider = self.loader.loadModel("data/models/vehicle01")
         # Reparent the model to render.
         self.glider.reparentTo(self.render)
         # Apply scale and position transforms on the model.
         self.glider.setScale(1, 1, 1)
-        self.glider.setPos(0, 0, 1)
+        self.glider.setPos(0, 0, 3)
         
+        myBody = OdeBody(world)
+        myBody.setPosition(self.glider.getPos(render))
+        myBody.setQuaternion(self.glider.getQuat(render))
+        myMass = OdeMass()
+        myMass.setBox(11340, 1, 1, 1)
+        myBody.setMass(myMass)
+        
+        
+        #set up the camera
         base.camera.reparentTo(self.glider)
         base.camera.setPos(0,-30,10)
-        base.camera.lookAt(self.glider)
-        #Initialise the Ode world
-        world = OdeWorld()
-        world.setGravity(0, 0, -9.81)
-         
+        base.camera.lookAt(self.glider)         
 
     def physicsTask(task):
       # Step the simulation and set the new positions
