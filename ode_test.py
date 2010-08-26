c146ef594d907fbb7d9afa38e106a84c34290132
Jannik <blazingsun@blazingsun-laptop>
2010-08-26 15:58:19 +0200

    added loading of the model and the world

--- ode_test.py
+++ ode_test.py
@@ -5,6 +5,29 @@ from pandac.PandaModules import *
 class RageTracks(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
+        base.disableMouse()
+
+        #load the models
+        
+        #environment
+        self.environ = self.loader.loadModel("data/models/egg/Plane")
+        # Reparent the model to render.
+        self.environ.reparentTo(self.render)
+        # Apply scale and position transforms on the model.
+        self.environ.setScale(1, 1, 1)
+        self.environ.setPos(0, 0, 0)
+        
+        #racer
+        self.glider = self.loader.loadModel("data/models/egg/gleiter_A")
+        # Reparent the model to render.
+        self.glider.reparentTo(self.render)
+        # Apply scale and position transforms on the model.
+        self.glider.setScale(1, 1, 1)
+        self.glider.setPos(0, 0, 1)
+        
+        base.camera.reparentTo(self.glider)
+        base.camera.setPos(0,-30,10)
+        base.camera.lookAt(self.glider)
         #Initialise the Ode world
         world = OdeWorld()
         world.setGravity(0, 0, -9.81)
