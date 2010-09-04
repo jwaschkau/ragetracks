6ab770a24b3e3518ba204cf9fed3c91961030e67
Carsten Pfeffer <tuschcarsten@web.de>
2010-09-04 13:37:43 +0200

    the skybox test module is now running the skybox is now an own picture - but we need good looking skyboxes, this one has some seams

--- skybox.py
+++ skybox.py
@@ -1,7 +1,31 @@
-# this is only a snippet
-self.skybox = loader.loadModel("data/models/skybox.egg")
-self.skybox.setBin("background", 1)
-self.skybox.setDepthWrite(0)
-self.skybox.setDepthTest(0)
-self.skybox.setLightOff()
-self.skybox.reparentTo(render)
+# _*_ coding: UTF-8 _*_
+
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import *
+
+# -----------------------------------------------------------------
+# -----------------------------------------------------------------
+# -----------------------------------------------------------------
+
+class Game(ShowBase):
+    '''
+    '''
+    def __init__(self):
+        '''
+        '''
+        ShowBase.__init__(self)
+        base.setFrameRateMeter(True) #Show the Framerate
+        self.skybox = self.loader.loadModel("data/models/skybox.egg")
+        self.skybox.setBin("background", 1)
+        self.skybox.setDepthWrite(0)
+        self.skybox.setDepthTest(0)
+        self.skybox.setLightOff()
+        self.skybox.setScale(2000)
+        self.skybox.reparentTo(render)
+
+
+
+# this code is only executed if the module is executed and not imported
+if __name__ == "__main__":
+    game = Game()
+    game.run()
