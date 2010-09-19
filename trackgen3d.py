56fcbc3b5d058dfa2ba2c8d0d837a2c10b87b77b
pinguin <pinguin@hb-nb07-linux>
2010-09-20 00:25:59 +0200

    cloud

--- trackgen3d.py
+++ trackgen3d.py
@@ -9,40 +9,39 @@
 from panda3d.core import * 
 from trackgen import Track
 
-def thickness(tupelOfTupel):
-    '''
-    '''
-    varthickness = []
-    #Generate the Vector for thickness of the road
-    
-    for i in range(len(tupelOfTupel)-1):
-        if i == 0:
-            varthickness.append(calcTheVector(tupelOfTupel[len(tupelOfTupel)-1],tupelOfTupel[i],tupelOfTupel[i+1]))
-            continue
-        varthickness.append(calcTheVector(tupelOfTupel[i-1],tupelOfTupel[i],tupelOfTupel[i+1]))
-    varthickness.append(calcTheVector(tupelOfTupel[len(tupelOfTupel)-2],tupelOfTupel[len(tupelOfTupel)-1],tupelOfTupel[0]))
-    
-    #Normalizing the Vector
-    for i in varthickness:
-        i.normalize()
+class Track3d(object):
+    def __init__(track_points, street_data):
+        '''
+        '''
+        varthickness = []
+        #Generate the Vector for thickness of the road
+        
+        for i in range(len(tupelOfTupel)-1):
+            if i == 0:
+                varthickness.append(calcTheVector(tupelOfTupel[len(tupelOfTupel)-1],tupelOfTupel[i],tupelOfTupel[i+1]))
+                continue
+            varthickness.append(calcTheVector(tupelOfTupel[i-1],tupelOfTupel[i],tupelOfTupel[i+1]))
+        varthickness.append(calcTheVector(tupelOfTupel[len(tupelOfTupel)-2],tupelOfTupel[len(tupelOfTupel)-1],tupelOfTupel[0]))
+        
+        #Normalizing the Vector
+        for i in varthickness:
+            i.normalize()
+        #Creating the Vertex
         ##TODO
-        #normalizing(i)
-    #Creating the Vertex
-    ##TODO
-    #Conect the Vertex
-    ##TODO
-    #?Show the Mesh
-    ##TODO
-    ##Debugprint
-    print varthickness
-    #return List
-    return varthickness
+        #Conect the Vertex
+        ##TODO
+        #?Show the Mesh
+        ##TODO
+        ##Debugprint
+        print varthickness
+        #return List
+        return varthickness
 
-def calcTheVector(pre, now, past):
-    vector1 = (pre[0] - now[0], pre[1] - now[1])
-    vector2 = (past[0] - now[0], past[1] - now[1])
-    high = pre[2] - past[2]
-    return Vec3(((vector1[1] + vector2[1])/2.0),((vector1[0] + vector2[0])/2.0), high)
+    def calcTheVector(pre, now, past):
+        vector1 = (pre[0] - now[0], pre[1] - now[1])
+        vector2 = (past[0] - now[0], past[1] - now[1])
+        high = pre[2] - past[2]
+        return Vec3(((vector1[1] + vector2[1])/2.0),((vector1[0] + vector2[0])/2.0), high)
 
 
 #Test
@@ -54,3 +53,5 @@ m.generateTrack()
 tuple4 = m.getInterpolatedPoints(100)
 print tuple4
 thickness(tuple4)
+
+streetData = (Vec2(1,1),Vec2(5,1),Vec2(6,2))
