6c64e480634caf1f51c8ac9a29130040b74d88e2
pinguin999 <pinguin999@gmail.com>
2010-09-26 00:15:40 +0200

    TRANSLATION TEST

--- menu.py
+++ menu.py
@@ -1,103 +1,13 @@
-# -*- coding: utf-8 -*-
-###################################################################
-## this module contains the menu class, which is used for the main-
-## and sub menus
-###################################################################
+import gettext
 
-from direct.gui.OnscreenImage import OnscreenImage
+#gettext.bindtextdomain('ragetracks', '/data/language/')
+#gettext.textdomain('ragetracks')
+#_ = gettext.gettext
 
-# -----------------------------------------------------------------
-# -----------------------------------------------------------------
-# -----------------------------------------------------------------
+#trans = gettext.translation('ragetracks', './data/language/', ['de']) 
+#trans.install()
+gettext.install('ragetracks', '/data/language/', ['de'])
+ 
 
-class Menu(object):
-    '''
-    '''
-    def __init__(self):
-        '''
-        '''
-        self.selection = 0
-        self.options = []
-        self.imagesDB = {#{name:[[unselected, selected],pos,scale]}
-        "NewGame":[["data/sprites/menu/newGame.png","data/sprites/menu/newGameSelected.png"],(0,0,0),(.6, 1, .06)]  
-        }       
-        self.selected = 0
-
-    # -----------------------------------------------------------------
-
-    def addOption(self, name, function):
-        '''
-        '''
-        image = OnscreenImage(self.imagesDB[name][0][0],self.imagesDB[name][1], self.imagesDB[name][2])
-        imageSelected = OnscreenImage(self.imagesDB[name][0][1],self.imagesDB[name][1], self.imagesDB[name][2])
-        self.options.append((name, function, image, imageSelected))
-
-    # -----------------------------------------------------------------
-
-    def hideMenu(self):
-        '''
-        '''
-        self.options.destroy()
-        pass
-
-    # -----------------------------------------------------------------
-
-    def selectNext(self):
-        '''
-        Kopie aus Hasenspiel
-        '''
-        old = self.selected
-        self.selected += 1
-        if self.selected == len(self.options):
-            self.selected = 0
-
-        if old < len(self.texts):
-            self.texts[old].setFg(self.color_deselected)
-        if self.selected < len(self.texts):
-            self.texts[self.selected].setFg(self.color_selected)
-
-    # -----------------------------------------------------------------
-
-    def selectPrev(self):
-        '''
-        Kopie aus Hasenspiel
-        '''
-        old = self.selected
-        self.selected -= 1
-        if self.selected == -1:
-            self.selected = len(self.options)-1
-
-        if old < len(self.texts):
-            self.texts[old].setFg(self.color_deselected)
-        if self.selected < len(self.texts):
-            self.texts[self.selected].setFg(self.color_selected)
-
-    # -----------------------------------------------------------------
-
-    def chooseOption(self):
-        '''
-        '''
-        # call the function behind the selected option
-        self.options[self.selection][1]()
-        '''
-        Kopie aus Hasenspiel
-        '''
-        i %= len(self.options)
-        old = self.selected
-        self.selected = i
-        if self.selected == -1:
-            self.selected = len(self.options)-1
-
-        if self.selected == len(self.options):
-            self.selected = 0
-
-        if old < len(self.texts):
-            self.texts[old].setFg(self.color_deselected)
-        if self.selected < len(self.texts):
-            self.texts[self.selected].setFg(self.color_selected)
-
-    # -----------------------------------------------------------------
-
-# -----------------------------------------------------------------
-# -----------------------------------------------------------------
-# -----------------------------------------------------------------
+print _("Please enter a value: ")
+print _("The random choice is: %s") % "hallo"
