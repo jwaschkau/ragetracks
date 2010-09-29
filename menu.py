44975cd805a3566c95dd2c4947edfeee0ff2fdd2
pinguin999 <pinguin999@gmail.com>
2010-09-29 02:24:26 +0200

    test

--- menu.py
+++ menu.py
@@ -1,7 +1,8 @@
 import gettext
 
-trans = gettext.translation("ragetrack", "data/language", ["de"]) 
-trans.install()
+#trans = gettext.translation("ragetrack", "data/language", ["de"]) 
+gettext.install("ragetrack", "data/language", unicode=True)
+
 
 print _("Please enter a value: ")
 print _("The random choice is: %s") % "hallo"
