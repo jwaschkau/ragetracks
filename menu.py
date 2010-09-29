76731b393a95be157a9e5d10fdf651e80b7d5476
pinguin999 <pinguin999@gmail.com>
2010-09-29 02:12:02 +0200

    gettext first ver

--- menu.py
+++ menu.py
@@ -1,21 +1,7 @@
 import gettext
 
-#gettext.bindtextdomain('ragetracks', '/data/language/')
-#gettext.textdomain('ragetracks')
-#_ = gettext.gettext
-
-#trans = gettext.translation('ragetracks', './tools/po/', ['de']) 
-#trans.install()
-
-#gettext.install('ragetracks', '/tools/po', ['de'])
-
-#gettext.bindtextdomain('ragetracks', '/tools/po')
-#gettext.textdomain('ragetracks')
-#_ = gettext.gettext
-print gettext.find("ragetrack")
-trans = gettext.translation("ragetrack", "/home/pinguin/RegaTracks/rangetracks/trunk", languages=["de","en"]) 
+trans = gettext.translation("ragetrack", "data/language", ["de"]) 
 trans.install()
-
 
 print _("Please enter a value: ")
 print _("The random choice is: %s") % "hallo"
