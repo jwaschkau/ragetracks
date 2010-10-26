6cc7b1578c64d5dd64d587b16eb3edcf34a4bd02
Jannik Waschkau <jannik@waschkau-bremen.de>
2010-10-26 12:57:42 +0200

    reduced shadow map resolution

--- menu.py
+++ menu.py
@@ -439,7 +439,7 @@ class Menu(object):
                     #ligt, that casts shadows
                     plight = Spotlight('plight')
                     plight.setColor(VBase4(10.0, 10.0, 10.0, 1))
-                    plight.setShadowCaster(True, 2048, 2048)#enable shadows for this light
+                    plight.setShadowCaster(True, 512, 512)#enable shadows for this light
                     plight.getLens().setFov(80)
                     plnp = vehicleSelectNode.attachNewNode(plight)
                     plnp.setPos(2, -10, 10)
